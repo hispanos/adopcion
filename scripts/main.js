6daf2c44eaa5afbd896bd42bbc0818e42b18cc62
hispanos <mailermartinez@gmail.com>
2021-06-03 00:18:13 -0500

    Show the pets ordered

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 const URI = window.location.pathname;
 if(URI === '/' || URI === '/index.html' || URI === '/adopcion/' || URI === 'adopcion/index.html') {
     setTimeout("window.location.href='./waiting-1.html'", 5000);
-    
 }
