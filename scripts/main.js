e72f52aca3bda6027f0549cfe8c2abfed15a46f1
hispanos <mailermartinez@gmail.com>
2021-06-02 22:41:11 -0500

    Add conditionals for github pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
 const URI = window.location.pathname;
-if(URI === '/' || URI === '/index.html') {
+if(URI === '/' || URI === '/index.html' || '/adopcion/' || 'adopcion/index.html') {
     setTimeout("window.location.href='./waiting-1.html'", 5000);
 }
