b2c7e510fa26d8e5438ed3c3a04bd2ada70ac4ad
hispanos <mailermartinez@gmail.com>
2021-06-03 17:32:42 -0500

    Render pets list by fetch

---
+++ scripts/Pets.js
@@ -0,0 +1,34 @@
+export default class Pets {
+
+    constructor() {
+        this.containerPetList = document.getElementById('container-pets-list');
+    }
+
+    async getPets(categoryId) {
+        const URI = '../db/pets.json'
+        const resp = await fetch(URI);
+        if (resp.ok) {
+            const {pets} = await resp.json();
+            const filter = pets.filter((item) => item.categoryId === categoryId)
+            return filter
+        }else{
+            return false;
+        }
+    }
+
+    renderListPets(listPets) {
+        this.containerPetList.innerHTML = '';
+        listPets.forEach(pet => {
+            const div = `
+            <div class="pet level-1">
+                <div class="background-pet"></div>
+                <img src="${pet.image}" alt="${pet.name}" class="img-pet">
+                <span class="pet-name">${pet.name}</span>
+                <span class="pet-race">${pet.race}</span>
+            </div>
+            `;
+            this.containerPetList.innerHTML += div;
+        });
+    }
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,36 @@
+import Pets from './Pets.js';
+
 const URI = window.location.pathname;
-if(URI === '/' || URI === '/index.html' || URI === '/adopcion/' || URI === 'adopcion/index.html') {
-    setTimeout("window.location.href='./waiting-1.html'", 5000);
+const pets = new Pets();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Validate URL for waiting index redirection
+    if(URI === '/' || URI === '/index.html' || URI === '/adopcion/' || URI === 'adopcion/index.html') {
+        setTimeout("window.location.href='./waiting-1.html'", 5000);
+    }
+
+    const category = document.querySelectorAll('.category');
+    category.forEach((div) => {
+        div.addEventListener('click', (e) => { onClickCategory(e) })
+    })
+    
+});
+
+const onClickCategory = (e) => {
+    const element = e.target;
+    let id = '';
+    if (element.id) {
+        id = element.id
+    }else if (element.parentElement.id) {
+        id = element.parentElement.id
+    }else if (element.parentElement.parentElement.id) {
+        id = element.parentElement.parentElement.id
+    }
+    getPetsByCategory(id);
+}
+
+const getPetsByCategory = async (categoryId) => {
+    const listPets = await pets.getPets(categoryId);
+    pets.renderListPets(listPets);
 }
