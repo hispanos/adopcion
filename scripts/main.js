a5338ab01125892fa128d685e7f78d9bbe891312
hispanos <mailermartinez@gmail.com>
2021-06-02 23:32:17 -0500

    Updated the conditional URI

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const URI = window.location.pathname;
-if(URI === '/' || URI === '/index.html' || '/adopcion/' || 'adopcion/index.html') {
+if(URI === '/' || URI === '/index.html' || URI === '/adopcion/' || URI === 'adopcion/index.html') {
     setTimeout("window.location.href='./waiting-1.html'", 5000);
+    
 }
