eafc749b75ea06966d9d5752f1da531e104c93d7
hispanos <mailermartinez@gmail.com>
2021-06-02 20:57:35 -0500

    Updated styles responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('container');
-    container.addEventListener('click', clickContainer)
-});
-
-const clickContainer = () => {
-    const imgSmall = document.getElementById('logo-small');
-    const imgBig = document.getElementById('logo-big');
-
-    imgSmall.classList.add('hidden');
-    imgBig.classList.remove('hidden');
-}
+setTimeout("window.location.href='./waiting-1.html'", 5000)
