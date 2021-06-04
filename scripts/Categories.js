38acb7263862790cbd7090f21f483690b29adeda
hispanos <mailermartinez@gmail.com>
2021-06-03 23:31:02 -0500

    Updated db Json

--- scripts/Categories.js
+++ scripts/Categories.js
@@ -6,7 +6,7 @@ export default class Categories {
     }
 
     async getCategories() {
-        const URI = '../db/categories.txt'
+        const URI = '../db/categories.json'
         const resp = await fetch(URI);
         if (resp.ok) {
             const {categories} = await resp.json();
